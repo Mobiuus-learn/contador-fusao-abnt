dbd1f3e2cb7ee7abf0ce466a2cb43f4cceed6bd6
Mobiuus-learn <63170559+Mobiuus-learn@users.noreply.github.com>
2023-01-14 08:42:47 -0300

    versao inicial

--- src/index.js
+++ src/index.js
@@ -3,120 +3,60 @@ const fFinal = document.querySelector("#f-final");
 const fieldSet = document.querySelectorAll("[name='fh']");
 const sendButton = document.querySelector(".calc-btn");
 
-const messages = {
-  quantityFiberError:
-    "A quantidade de fibra inicial não pode ser superior a final.",
-  radioButtonUnchecked: "Selecione o cabo alimentador correspondente. ",
-
-  capacityCableOverrated:
-    "A quantidade de fusões nao pode exceder a capacidade do cabo alimentador",
-};
-
-
 const possibleColors = ['verde', 'amarelo', 'branco', 'azul', 'vermelho',
     'violeta','marrom', 'rosa', 'preto', 'cinza', 'laranja', 'aqua']
 
+function mainCall(){
+  getRadioValue()
+  calcPrimary()
 
-//Captura o valor do radio (quantidade de fibras no cabo alimentador)
+}
 function getRadioValue(){
+  let result 
   for (let index = 0; index < fieldSet.length; index++) {
     const element = fieldSet[index];
     if (element.checked) {
-      // console.log(element.value);
-      return Number(element.value);
+      result = element.value
+      return result;
     }
     }
   }
 
-const checkInputField = ()=>{
-  if(fInitial || fFinal === '' ){
-    console.log('Error, informe as fibras iniciais e finais;')
-    return false
-  }
-  return true
+
+
+const groups = {
+  '12':1,
+  '24':4,
+  '36':6,
+  '48':4,
+  '72':6,
+  '144':12
 }
-const calcFusionTotal = async (f1, f2) => {
-  let result =  await f2 - f1 + 1;
-  return result
-};
 
-const calcFusion = async () => {
-  let f1 = Number(fInitial.value);
-  let f2 = Number(fFinal.value);
-
-  let fhResult = getRadioValue();
-  let fTotal = await calcFusionTotal(f1, f2);
-  let groups = calcGroups(fhResult)
-
-  if(!checkInputField){
-    console.log(error)
-  }
-  if (fhResult === undefined) {
-    alert(messages.radioButtonUnchecked);
-
-    return true;
-  }
-  if (fhResult < calcFusionTotal) {
-    alert(messages.capacityCableOverrated);
-    return true;
-  }
-
-  if (f2 < f1) {
-    alert(messages.quantityFiberError);
-    return true;
-  }
-
-  console.log(fTotal, groups); 
-  return fTotal
-
-};
- function calcGroups(fMain){
-  let groupM = Number(fMain);
-  let fiberPerGroup;
-  let groups;
-
-  switch (groupM) {
-    case 12: {
-      fiberPerGroup = 12;
-      groups = Math.ceil(groupM / fiberPerGroup);
-      break;
-    }
-    case 24: {
-      fiberPerGroup = 6;
-      groups = Math.ceil(groupM / fiberPerGroup);
-      break;
-    }
-
-    case 36: {
-      fiberPerGroup = 6;
-      groups = Math.ceil(groupM / fiberPerGroup);
-      break;
-    }
-    case 48: {
-      fiberPerGroup = 12;
-      groups = Math.ceil(groupM / fiberPerGroup);
-      break;
-    }
-    case 72: {
-      fiberPerGroup = 12;
-      groups = Math.ceil(groupM / fiberPerGroup);
-      break;
-    }
-    case 144: {
-      fiberPerGroup = 12;
-      groups = Math.ceil(groupM / fiberPerGroup);
-      break;
-    }
-  }
-  return groups;
-};
-const calcColorFusions = ()=>{
- let totalDeFusoes = calcFusionTotal()
-  for(i = 0; i <= totalDeFusoes; i++){
-    console.log(possibleColors[i])
-  }
-  console.log(totalDeFusoes)
+const calcPrimary = async () =>{
+  let fibIn = Number(fInitial.value)
+  let fibFin = Number(fFinal.value)
+  let calcFusion = fibFin - fibIn + 1 
+  let groupsC = groupsPerCable()
+  
+  let groupsPerFusion = Math.ceil(calcFusion / groupsC)
+  console.log(`o total de fusões é: ${calcFusion}; O total de grupos é ${groupsPerFusion}`)
 }
-// sendButton.onclick = getRadioValue
-// sendButton.onclick = calcFusion;
-sendButton.onclick =calcFusion, calcColorFusions;
+
+
+function groupsPerCable() {
+  
+  let valueInput = getRadioValue();
+  let result;
+  for(const prop in groups){
+     if(prop == valueInput){
+      result = groups[prop]
+    }
+  }
+   
+  return Number(result);
+}
+  
+
+
+sendButton.onclick = mainCall
